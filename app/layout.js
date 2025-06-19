92d83639e9a958afa9d9a39f8905eaac3f80b722
Eren <eerenn327@hotmail.com>
2025-06-19 17:56:12 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,15 @@
 import '@/styles/globals.css'
 import { AuthProvider } from '@/context/AuthContext';
+import { ProductProvider } from '@/context/ProductContext';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <AuthProvider>
-          {children}
+          <ProductProvider>
+            {children}
+          </ProductProvider>
         </AuthProvider>
       </body>
     </html>
