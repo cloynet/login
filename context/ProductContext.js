18395fa0160ff7335e45fc711d7a1977a2784bda
Eren <eerenn327@hotmail.com>
2025-06-23 16:04:56 +0300

    Update ProductContext.js

--- context/ProductContext.js
+++ context/ProductContext.js
@@ -1,29 +1,30 @@
 'use client';
 
 import { createContext, useContext, useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
+    const pathname = usePathname();
     const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+        if (pathname === '/products') {
+            fetchProducts();
+        }
+    }, [pathname]);
 
     const fetchProducts = async () => {
         const res = await fetch('https://dummyjson.com/products');
         const data = await res.json();
-        console.log(data.products);
+        // console.log(data.products);
         setProducts(data.products);
     };
 
-    const getProductById = (id) => {
-        return products.find((p) => p.id === Number(id));
-    };
-
-    useEffect(() => {
-        fetchProducts();
-    }, []);
-
     return (
-        <ProductContext.Provider value={{ products, getProductById }}>
+        <ProductContext.Provider value={{ products }}>
             {children}
         </ProductContext.Provider>
         )
